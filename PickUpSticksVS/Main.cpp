34a6937009d030b2de444cb6cdd46174fc019730
Charlie-Foy-117 <83784786+Charlie-Foy-117@users.noreply.github.com>
2023-01-11 15:52:49 +0000

    Created grass vector and added random num gen
    currently for loop places grass always in same position as each other

--- PickUpSticksVS/Main.cpp
+++ PickUpSticksVS/Main.cpp
@@ -1,23 +1,123 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pick Up Sticks", sf::Style::None);
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //------------------------------------------------------------
+    // Setup
+    //------------------------------------------------------------
 
+#pragma region Setup
+
+
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Pick Up Sticks", sf::Style::None);
+
+    sf::Texture playerTexture;
+    sf::Texture grassTexture;
+
+    int randomNumGenX, randomNumGenY;
+
+    /* initialize random seed: */
+    srand(time(NULL));
+
+    /* generate secret number between 1 and 10: */
+    randomNumGenX = rand() % (window.getSize().x - grassTexture.getSize().x);
+    randomNumGenY = rand() % (window.getSize().y - grassTexture.getSize().y);
+
+
+
+    if (!playerTexture.loadFromFile("Assets/Player_Stand.png"))
+    {
+        //error
+        std::cout << "Texture load failed for Assets/Player_Stand.png" << std::endl;
+    }
+    else
+    {
+        std::cout << "Texture load was successful for Assets/Player_Stand.png" << std::endl;
+    }
+
+    if (!grassTexture.loadFromFile("Assets/Grass.png"))
+    {
+        //error
+        std::cout << "Texture load failed for Assets/Grass.png" << std::endl;
+    }
+    else
+    {
+        std::cout << "Texture load was successful for Assets/Grass.png" << std::endl;
+    }
+
+    sf::Sprite playerSprite;
+    sf::Sprite grassSprite;
+    grassSprite.setTexture(grassTexture);
+    playerSprite.setTexture(playerTexture);
+    std::vector<sf::Sprite> grassSpriteVector;
+    int numGrassSprites = 5;
+
+    for (size_t i = 0; i < numGrassSprites; i++)
+    {
+        grassSpriteVector.push_back(grassSprite);
+    }
+
+    //postion
+    playerSprite.setPosition(sf::Vector2f(0.0f, 100.0f));
+
+#pragma endregion
+
+    
     while (window.isOpen())
     {
+
+    //------------------------------------------------------------
+    // Event Polling
+    //------------------------------------------------------------
+
+#pragma region Event Polling
+
+
         sf::Event event;
         while (window.pollEvent(event))
         {
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+                    window.close();
+                }
+            }
+
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+
+#pragma endregion
+
+
+    //------------------------------------------------------------
+    // Drawing
+    //------------------------------------------------------------
+
+#pragma region Drawing
+
+
+        window.clear(sf::Color(60, 150, 64));
+
+        //draw all things
+        for (size_t i = 0; i < grassSpriteVector.size(); i++)
+        {
+            grassSpriteVector[i].setPosition(sf::Vector2f(randomNumGenX, randomNumGenY));
+            window.draw(grassSpriteVector[i]);
+        }
+        window.draw(playerSprite);
+
         window.display();
+
+
+#pragma endregion
+
     }
 
     return 0;
